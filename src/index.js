debc7ab3ff87d43c0576118d930cbfdcfb98bc27
aerobis <185216831+aerobis@users.noreply.github.com>
2026-03-31 21:16:22 +0545

    Save current local changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import {todoListMaker} from "./todo-section.js";
 document.addEventListener("DOMContentLoaded", ()=>{
     let projectContainer = document.querySelector(".project-section");
     let todoContainer = document.querySelector(".todo-section");
+    let todoForm = document.querySelector("#todo-form");
+    let newTodoBtn = document.querySelector("#new-todo-button");
+    let todoSubmitBtn = document.querySelector("#todo-submit-button");
+    let todoCancelBtn = document.querySelector("#todo-cancel-button");
 
     projectListMaker(projectContainer);
     todoListMaker(todoContainer);
+
+    console.log(newTodoBtn);
+
+    newTodoBtn.addEventListener("click", ()=>{
+        todoForm.style.display = "block";
+    });
 });
